f77df8497ba7b0f6f8ab32db2d79f2714b53d8bb
Timothy Jones <tyjones1@student.fullsail.edu>
2024-01-14 16:59:55 -0500

    added string test

---
+++ string/string.js
@@ -0,0 +1,11 @@
+const uppercase = (str) => {
+    return str.toUpperCase();}
+
+const lowercase = (str) => {
+    return str.toLowerCase();}
+
+    //lowercase function to lowercase text
+module.exports = {
+    uppercase, 
+    lowercase
+}

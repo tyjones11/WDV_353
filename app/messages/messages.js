c31df9d5be1eef8abf1cd44382790c393d2024de
Timothy Jones <tyjones1@student.fullsail.edu>
2024-02-04 12:48:30 -0500

    Created messages module

---
+++ app/messages/messages.js
@@ -0,0 +1,12 @@
+module.exports = {
+    movie_not_found : "Movie Not Found",
+    movie_deleted : "Movie Deleted",
+    movie_saved : "Movie Saved To Collection",
+    movie_already_cataloged : "Movie Already Cataloged",
+    movie_updated : "Movie Updated Successfully",
+    director_not_found : "Director Not Found",
+    director_deleted : "Director Deleted",
+    director_saved : "Director Saved To Collection",
+    director_already_cataloged : "Director Already Cataloged",
+    director_updated : "Director Updated Successfully"
+}
